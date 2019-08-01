d8c57079a5e75d8c80ef242e5e8420817c6f526a
phoenixcoder <thephoenixcoder@gmail.com>
2019-08-01 00:05:26 -0700

    Add separate registry logic.

---
+++ umbrella/registry.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/phoenixcoder/slack-golang-sdk/slashcmd"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const (
+	cmdNotRegErrFmt    = "Command is not registered. Command Name: '%s'"
+	noArgsErrFmt       = "No arguments received."
+	logRegUrl          = "Registry URL: %s\n"
+	errDlRegFmt        = "Could not download registry contents. Error: %v\n"
+	errReadRegFmt      = "Could not read registry contents. Error: %v\n"
+	errFuncNotFoundFmt = "We're embarassed for you, but we don't know a '%s'."
+)
+
+type CommandNotFoundError error
+type FunctionNotFoundError error
+type ArgsNotFoundError error
+type commandRegistry map[string]commandRecord
+type functionRegistry map[string]functionRecord
+
+type commandRecord struct {
+	ReservedKeywords []string         `json:"reservedKeywords"`
+	Functions        functionRegistry `json:"functions"`
+}
+
+type functionRecord struct {
+	// Usage is a description of how to use the function with the command.
+	Usage string `json: "usage"`
+	// Description is a description of what the function does.
+	Description string `json: "description"`
+	// Manual is a location for additional information on the function.
+	Manual string `json: "manual"`
+}
+
+func (cr *commandRegistry) getFunctionRecord(cmd *slashcmd.Info) (*functionRecord, error) {
+	cmdRec, cmdRecOk := (*cr)[strings.ToLower(cmd.Command)]
+	if !cmdRecOk {
+		return nil, CommandNotFoundError(fmt.Errorf(cmdNotRegErrFmt, cmd.Command))
+	}
+
+	if len(cmd.Arguments) <= 0 {
+		return nil, ArgsNotFoundError(errors.New(noArgsErrFmt))
+	}
+	funcName := cmd.Arguments[0]
+	funcRec, funcRecOk := cmdRec.Functions[strings.ToLower(funcName)]
+	if !funcRecOk {
+		return nil, FunctionNotFoundError(fmt.Errorf(errFuncNotFoundFmt, funcName))
+	}
+
+	return &funcRec, nil
+}
+
+func NewCommandRegistry(contents []byte) (*commandRegistry, error) {
+	var cr commandRegistry
+	if err := json.Unmarshal(contents, &cr); err != nil {
+		return nil, err
+	}
+
+	return &cr, nil
+}
+
+func NewCommandRegistryFromFile(location string) (*commandRegistry, error) {
+	contents, err := ioutil.ReadFile(location)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewCommandRegistry(contents)
+}
+
+func NewCommandRegistryFromUrl(url string) (*commandRegistry, error) {
+	log.Printf(logRegUrl, url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf(errDlRegFmt, err)
+		return nil, err
+	}
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf(errReadRegFmt, err)
+		return nil, err
+	}
+
+	return NewCommandRegistry(contents)
+}
---
+++ umbrella/registry_test.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"github.com/phoenixcoder/slack-golang-sdk/slashcmd"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	testRegJson = `{
+                           "command1": {
+                               "reservedKeywords" : ["reservedKeywords1"],
+                               "functions" : {
+                                   "functions1" : {
+                                       "usage" : "usage1",
+                                       "description" : "description1",
+                                       "manual" : "manual1"
+                                   }
+                               }
+                           },
+                           "command2": {
+                               "reservedKeywords" : ["reservedKeywords2"],
+                               "functions" : {
+                                   "functions2" : {
+                                       "usage" : "usage2",
+                                       "description" : "description2",
+                                       "manual" : "manual2"
+                                   }
+                               }
+                           }
+                       }`
+	testCommand     = "command"
+	testResKeywords = "reservedKeywords"
+	testFunctions   = "functions"
+	testUsage       = "usage"
+	testDesc        = "description"
+	testMan         = "manual"
+)
+
+func TestGetFunctionRecord(t *testing.T) {
+	fmtStr := "%s%d"
+	cmdReg, err := NewCommandRegistry([]byte(testRegJson))
+	assert.Nil(t, err)
+
+	for i := 1; i <= 2; i++ {
+		cmd := &slashcmd.Info{
+			Command:   fmt.Sprintf(fmtStr, testCommand, i),
+			Arguments: []string{fmt.Sprintf(fmtStr, testFunctions, i)},
+		}
+		assert.Contains(t, (*cmdReg)[cmd.Command].ReservedKeywords, fmt.Sprintf(fmtStr, testResKeywords, i))
+		funcRec, err := cmdReg.getFunctionRecord(cmd)
+		assert.Nil(t, err)
+		assert.Equal(t, funcRec.Usage, fmt.Sprintf(fmtStr, testUsage, i))
+		assert.Equal(t, funcRec.Description, fmt.Sprintf(fmtStr, testDesc, i))
+		assert.Equal(t, funcRec.Manual, fmt.Sprintf(fmtStr, testMan, i))
+	}
+}
+
+func TestLoadRegistryFromMalformedContents(t *testing.T) {
+	testMalRegJson := `{
+           malformed
+        }`
+	cmdReg, err := NewCommandRegistry([]byte(testMalRegJson))
+	assert.Nil(t, cmdReg)
+	assert.NotNil(t, err)
+}
+
+func TestCommandNotFoundError(t *testing.T) {
+	cmdReg := make(commandRegistry)
+
+	cmd := &slashcmd.Info{}
+	funcRec, err := cmdReg.getFunctionRecord(cmd)
+	assert.Nil(t, funcRec)
+	assert.NotNil(t, err)
+
+	_, ok := err.(CommandNotFoundError)
+	assert.True(t, ok)
+}
+
+func TestFunctionNotFoundError(t *testing.T) {
+	cmdReg, err := NewCommandRegistry([]byte(testRegJson))
+	assert.Nil(t, err)
+	cmd := &slashcmd.Info{
+		Command:   testCommand,
+		Arguments: []string{testFunctions},
+	}
+	funcRec, err := cmdReg.getFunctionRecord(cmd)
+	assert.Nil(t, funcRec)
+	assert.NotNil(t, err)
+
+	_, ok := err.(FunctionNotFoundError)
+	assert.True(t, ok)
+}
+
+func TestArgsNotFoundError(t *testing.T) {
+	cmdReg := make(commandRegistry)
+
+	cmd := &slashcmd.Info{
+		Command: testCommand,
+	}
+	funcRec, err := cmdReg.getFunctionRecord(cmd)
+	assert.Nil(t, funcRec)
+	assert.NotNil(t, err)
+
+	_, ok := err.(ArgsNotFoundError)
+	assert.True(t, ok)
+}
