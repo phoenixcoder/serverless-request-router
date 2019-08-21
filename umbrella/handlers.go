897589db8a71fa226de5b6446a7aeb36ea6d8c00
phoenixcoder <thephoenixcoder@gmail.com>
2019-08-20 23:15:21 -0700

    Add ProxyHandler.

---
+++ umbrella/handlers.go
@@ -0,0 +1,101 @@
+package router
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const (
+	requestUrl        = "request-url"
+	contentTypeHeader = "content-type"
+
+	// TODO Move to a separate package.
+	forbiddenErrRespMsg   = "uh uh uh...you didn't say the magic word."
+	internalErrRespMsg    = "Sorry...we uh...messed up."
+	funcNotFoundErrMsgFmt = "We're embarrassed for you, but we don't know a '%s'. Try these instead:\n'%s'"
+	logMsg                = "%s, %s"
+	ErrorKey              = "error"
+	TaskBody              = "body"
+)
+
+// TODO Move to a separate package.
+func setInternalErrCode(task *TaskMap, reason string) {
+	setErredStatusCode(task, internalErrRespMsg, reason, http.StatusInternalServerError)
+}
+
+// TODO Move to a separate package.
+func setForbiddenErrCode(task *TaskMap) {
+	setErredStatusCode(task, forbiddenErrRespMsg, "You're just not allowed.", http.StatusForbidden)
+}
+
+// TODO Move to a separate package.
+func setErredStatusCode(task *TaskMap, msg string, reason string, statusCode int) {
+	(*task)["StatusCode"] = statusCode
+	(*task)["Body"] = msg + " (" + http.StatusText(statusCode) + ")"
+	log.Printf(logMsg, (*task)["Body"], reason)
+}
+
+type httpClientInterface interface {
+	Post(url string, contentType string, body io.Reader) (*http.Response, error)
+}
+
+// ProxyHandler is a wrapper for the http client and process that
+// forwards on the request to the intended service.
+type ProxyHandler struct {
+	errMsg            string
+	requestUrl        string
+	contentTypeHeader string
+	client            httpClientInterface
+}
+
+// NewProxyHandler is a factory method for creating the proxy handler with
+// a separately configured http client.
+func NewProxyHandler(client httpClientInterface) ProxyHandler {
+	return ProxyHandler{
+		errMsg: "Request url was not provided when proxying.",
+		client: client,
+	}
+}
+
+// Before method that does nothing.
+func (p *ProxyHandler) Before(context *ContextMap, task *TaskMap) bool {
+	return false
+}
+
+// Execute method that inspects the context for a request url and sends
+// an http request to that url. It sends the response of the request
+// back in the task.
+func (p *ProxyHandler) Execute(context *ContextMap, task *TaskMap) {
+	body := (*task)[TaskBody]
+	bodyStr, _ := body.(string)
+	contentType := (*context)[contentTypeHeader]
+	contentTypeStr, _ := contentType.(string)
+	requestUrl, requestUrlOk := (*context)[requestUrl]
+	requestUrlStr, requestUrlStrOk := requestUrl.(string)
+	requestUrlOk = requestUrlOk && requestUrlStrOk
+	if requestUrlOk {
+		routedResp, err := p.client.Post(requestUrlStr, contentTypeStr, strings.NewReader(bodyStr))
+		if err != nil {
+			(*task)[ErrorKey] = err
+			return
+		}
+
+		routedRespBody, err := ioutil.ReadAll(routedResp.Body)
+		if err != nil {
+			(*task)[ErrorKey] = err
+			return
+		}
+
+		(*task)[TaskBody] = string(routedRespBody)
+		return
+	}
+
+	(*task)[ErrorKey] = errors.New(p.errMsg)
+}
+
+// After method that does nothing.
+func (p *ProxyHandler) After(context *ContextMap, task *TaskMap) {}
---
+++ umbrella/handlers_test.go
@@ -0,0 +1,149 @@
+package router
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+const (
+	testProxyUrl         = "https://testurl.com/"
+	testProxyBody        = "Test Proxy Body"
+	testProxyContentType = "application/json"
+)
+
+type mockHttpClient struct {
+	mock.Mock
+}
+
+type mockReader struct {
+	mock.Mock
+}
+
+func (m *mockReader) Read(p []byte) (int, error) {
+	args := m.Called(p)
+	return args.Int(0), args.Error(1)
+}
+
+func (m *mockReader) Close() error {
+	args := m.Called()
+
+	return args.Error(0)
+}
+
+func (m *mockHttpClient) Post(url string, contentType string, body io.Reader) (*http.Response, error) {
+	args := m.Called(url, contentType, body)
+	return args[0].(*http.Response), args.Error(1)
+}
+
+func TestProxyHandler(t *testing.T) {
+	testCtx := make(ContextMap)
+	testTask := make(TaskMap)
+	testResp := &http.Response{
+		Body: ioutil.NopCloser(strings.NewReader(testProxyBody)),
+	}
+	mHttpClient := new(mockHttpClient)
+
+	testCtx[requestUrl] = testProxyUrl
+	testCtx[contentTypeHeader] = testProxyContentType
+
+	testHandler := NewProxyHandler(mHttpClient)
+
+	mHttpClient.On("Post", mock.Anything, mock.Anything, mock.Anything).Return(testResp, nil)
+
+	testHandler.Execute(&testCtx, &testTask)
+
+	assert.Equal(t, testTask[TaskBody], testProxyBody)
+	assert.Nil(t, testTask[ErrorKey])
+	assert.Equal(t, testCtx[requestUrl], testProxyUrl)
+	assert.Equal(t, testCtx[contentTypeHeader], testProxyContentType)
+
+	mHttpClient.AssertNumberOfCalls(t, "Post", 1)
+}
+
+func TestProxyHandlerPostError(t *testing.T) {
+	testError := errors.New("Post Error")
+	testCtx := make(ContextMap)
+	testTask := make(TaskMap)
+	testResp := &http.Response{}
+	mHttpClient := new(mockHttpClient)
+	mReader := new(mockReader)
+	testCtx[requestUrl] = testProxyUrl
+	testCtx[contentTypeHeader] = testProxyContentType
+	testResp.Body = mReader
+	testHandler := NewProxyHandler(mHttpClient)
+
+	mHttpClient.On("Post", mock.Anything, mock.Anything, mock.Anything).Return(testResp, testError)
+	mReader.On("Read", mock.Anything).Return(0, nil)
+
+	testHandler.Execute(&testCtx, &testTask)
+
+	assert.False(t, testHandler.Before(nil, nil))
+	assert.Nil(t, testTask[TaskBody])
+	assert.NotNil(t, testTask[ErrorKey])
+	assert.Equal(t, testTask[ErrorKey], testError)
+	assert.Equal(t, testCtx[requestUrl], testProxyUrl)
+	assert.Equal(t, testCtx[contentTypeHeader], testProxyContentType)
+
+	mHttpClient.AssertNumberOfCalls(t, "Post", 1)
+	mReader.AssertNotCalled(t, "Read", mock.Anything)
+}
+
+func TestProxyHandlerReadingError(t *testing.T) {
+	testError := errors.New("Reading Error")
+	testCtx := make(ContextMap)
+	testTask := make(TaskMap)
+	testResp := &http.Response{}
+	mHttpClient := new(mockHttpClient)
+	mReader := new(mockReader)
+	testCtx[requestUrl] = testProxyUrl
+	testCtx[contentTypeHeader] = testProxyContentType
+	testResp.Body = mReader
+	testHandler := NewProxyHandler(mHttpClient)
+
+	mHttpClient.On("Post", mock.Anything, mock.Anything, mock.Anything).Return(testResp, nil)
+	mReader.On("Read", mock.Anything).Return(0, testError)
+
+	testHandler.Execute(&testCtx, &testTask)
+
+	assert.False(t, testHandler.Before(nil, nil))
+	assert.Nil(t, testTask[TaskBody])
+	assert.NotNil(t, testTask[ErrorKey])
+	assert.Equal(t, testTask[ErrorKey], testError)
+	assert.Equal(t, testCtx[requestUrl], testProxyUrl)
+	assert.Equal(t, testCtx[contentTypeHeader], testProxyContentType)
+
+	mHttpClient.AssertNumberOfCalls(t, "Post", 1)
+	mReader.AssertNumberOfCalls(t, "Read", 1)
+}
+
+func TestProxyHandlerNoRequestUrl(t *testing.T) {
+	testCtx := make(ContextMap)
+	testTask := make(TaskMap)
+	testResp := &http.Response{}
+	mHttpClient := new(mockHttpClient)
+	mReader := new(mockReader)
+	testCtx[contentTypeHeader] = testProxyContentType
+	testResp.Body = mReader
+	testHandler := NewProxyHandler(mHttpClient)
+
+	mHttpClient.On("Post", mock.Anything, mock.Anything, mock.Anything).Return(testResp, nil)
+	mReader.On("Read", mock.Anything).Return(0, nil)
+
+	testHandler.Execute(&testCtx, &testTask)
+
+	assert.False(t, testHandler.Before(nil, nil))
+	assert.Nil(t, testTask[TaskBody])
+	assert.Nil(t, testCtx[requestUrl])
+	assert.NotNil(t, testTask[ErrorKey])
+	assert.Equal(t, testCtx[contentTypeHeader], testProxyContentType)
+	assert.Equal(t, testTask[ErrorKey].(error).Error(), testHandler.errMsg)
+
+	mHttpClient.AssertNotCalled(t, "Post", mock.Anything, mock.Anything, mock.Anything)
+	mReader.AssertNotCalled(t, "Read", mock.Anything)
+}
